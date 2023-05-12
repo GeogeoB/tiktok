3f8eb0a928d281986c157495d2c2b455d7014495
Geoffrey <geoffrey.brenne@gmail.com>
2023-05-12 16:51:00 +0200

    modifié :         src/config.js 	modifié :         src/css/leftScreen.css

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const urlJboss = "https://192.168.27.88:8443/Application_Web_Projet_Backend";
-const urlJboss = "https://192.168.42.41:8443/TiktokBackend";
+const urlJboss = "https://192.168.43.146:8443/TiktokBackend";
 
 export default urlJboss;
