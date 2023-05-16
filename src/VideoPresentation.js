05c0ff76d2b7994af4125c5e9ad1fa2f565a9428
djo2000 <geoffrey.brenne@gmail.com>
2023-05-16 22:58:44 +0200

    modified:   src/VideoPresentation.js 	modified:   src/css/index.css

--- src/VideoPresentation.js
+++ src/VideoPresentation.js
@@ -1,32 +1,74 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./css/index.css";
 import { appContext } from "./context";
 import Play from "./icones/play";
+import Video from "./Video";
 
 function VideoPresentation() {
   let videos = [<video src={"info.src"} className="video" controls></video>];
-
-  let info = {
-    src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-    nb_vue: 200,
-  };
 
   let context = useContext(appContext);
   let videoPresentationInfo = context.videoPresentationInfo;
   let setVideoPresentationInfo = context.setVideoPresentationInfo;
 
+  let [videoViewerOpen, setvideoViewerOpen] = useState(false);
+
+  function scrollToTop() {
+    const currentPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentPosition - currentPosition / 8); // Adjust the division value for desired scrolling speed
+    }
+  }
+
+  let videoClickekd = () => {
+    setvideoViewerOpen(true);
+    scrollToTop();
+  };
+
+  let close = () => {
+    setvideoViewerOpen(false);
+  };
+
   let VideoItem = () => (
-    <div className="videoExploration-videos">
+    <div className="videoExploration-videos" onClick={videoClickekd}>
       <div className="info-video">
         <Play></Play>
-        <p className="infoVue">{info.nb_vue}</p>
+        <p className="infoVue">200</p>
       </div>
       <video src={info.src} className="video-column" autoPlay={false}></video>
     </div>
   );
 
+  const info = {
+    src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    play: false,
+    user: "@mia.aroundtheworld",
+    userPicture: "",
+    like: false,
+    id: 0,
+    pos: 0,
+    place: "Eiffel Tower View, Paris, France",
+    hashtags: ["France", "Paris", "Architecture"],
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum provident corrupti fuga quae, beatae cum deleniti maiores. Maiores unde rem vel esse velit dolorem dolore, labore, reiciendis delectus necessitatibus omnis",
+    nb_like: 500,
+    nb_commentaire: 500,
+  };
+
+  const VideoViewer = () => (
+    <div className="loginbg">
+      <div className="videoSlider video-video-presentation">
+        <div className="video-close close-button" onClick={close}></div>
+        <Video info={info}></Video>
+      </div>
+    </div>
+  );
+
   return (
     <>
+      {videoViewerOpen && <VideoViewer />}
       <div className="header-videopresentation">
         <div className="video-header">
           <div className="circle_abonnement_pres pp_comments_circle">
@@ -61,6 +103,41 @@ function VideoPresentation() {
       </div>
 
       <div className="videoExploration">
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
+        <VideoItem></VideoItem>
         <VideoItem></VideoItem>
         <VideoItem></VideoItem>
         <VideoItem></VideoItem>
