5a33c8891d57e1de33277f864e2e2804be4b5469
Geoffrey <geoffrey.brenne@gmail.com>
2023-05-30 13:54:37 +0200

    modifié :         src/config.js 	modifié :         src/icones/heart.js 	modifié :         src/index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { MyContextProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <MyContextProvider>
       <App />
     </MyContextProvider>
-  </React.StrictMode>
 );
